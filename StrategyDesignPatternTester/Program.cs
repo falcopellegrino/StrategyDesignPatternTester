507f52178ff1c1323bd5e8b50927720e397eceee
falcopellegrino <franco.pellegrino@aruba.it>
2017-03-05 16:50:08 +0100

    Aggiungere i file di progetto.

---
+++ StrategyDesignPatternTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDesignPatternTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
